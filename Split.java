94e241276d0a33e527f0625b5cea64f25b1f3350
kameshgv <97147189+kameshgv@users.noreply.github.com>
2022-01-07 13:01:06 +0530

    updated

--- Split.java
+++ Split.java
@@ -1,33 +1,28 @@
 package Assignment6;
-import java.util.Scanner;
-public class Split {
-public static void main(String[] args) {
-		
-				Scanner sc = new Scanner(System.in);
-				System.out.println("enter the string to be splitted");
-				String str = sc.nextLine();
-				System.out.println("enter the breakpoint character");
-				char character = sc.next().charAt(0);
-				sc.close();
-				int length = str.length();
-				char []values = new char[length];
-				for(int i = 0; i < length; i++) {
-					if(str.charAt(i) == character) 
-					{
-						str.replace(str.toString(),"");
-						System.out.println();
-					}
-					else
-					{
-						values[i]=str.charAt(i);
-					}
-					}
-				
-				for(char value : values) {
-					System.out.print(value);
-				}}}
-				
-			
-		
 
+import java.util.Scanner;
+
+public class Split {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("enter the string to be splitted ");
+		String tobeSplitted=scan.nextLine();
+		String breakAt="abc";
+		scan.close();
+		Mysplit(tobeSplitted,breakAt);
+		}
+	static void Split(String tobeSplitted,String breakAt )
+	{
+		for(int i=0; i<=tobeSplitted.length();i++)
+		{
+			if (tobeSplitted.charAt(i)==' ')
+			{
+				System.out.println("");
+				continue;
+			}
+			System.out.println(tobeSplitted.charAt(i));
+		}
+	}
+}
 
